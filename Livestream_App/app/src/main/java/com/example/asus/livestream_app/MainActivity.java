3d81e793b4e468ecaaea793f0e2df888a061259a
Kurosaki12a1 <tyblackzs@gmail.com>
2018-06-18 17:10:37 +0700

    Update PopUp

--- Livestream_App/app/src/main/java/com/example/asus/livestream_app/MainActivity.java
+++ Livestream_App/app/src/main/java/com/example/asus/livestream_app/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.asus.livestream_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
